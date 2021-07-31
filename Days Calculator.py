cd00301ec96f2fe5847da46d1e2afdf29db25577
Shraddha Kakade <71275041+ShraddhaKakade@users.noreply.github.com>
2021-07-31 22:53:41 +0530

    Add files via upload

---
+++ Days Calculator.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+
+def main():
+    today = datetime.today()
+    birth_date = input("Enter date in YYYY-MM-DD format : ")
+    date = datetime.strptime(birth_date, "%Y-%m-%d")
+
+    if date.month > today.month:
+        print(f"\nYou will turn {today.year - date.year} this year 🎂 !")
+    else:
+        print(f"\nYou had {today.year - date.year} birthdays 🎁 !")
+
+    print(f"\nYou are "
+          f"\n{(today - date).days} days "
+          f"\n{(today.hour - date.hour)} hours "
+          f"\n{(today.minute - date.minute)} minutes and"
+          f"\n{(today.second - date.second)} seconds "
+          f"\nold !")
+
+
+if __name__ == "__main__":
+    main()
