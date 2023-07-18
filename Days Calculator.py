a8c9f4ff413da5b1ae1cd64b826c1d9a52be06ca
Shraddha Kakade <71275041+ShraddhaKakade@users.noreply.github.com>
2023-07-18 16:43:32 +0530

    Update Days Calculator.py

--- Days Calculator.py
+++ Days Calculator.py
@@ -4,19 +4,24 @@ from datetime import datetime
 def main():
     today = datetime.today()
     birth_date = input("Enter date in YYYY-MM-DD format : ")
-    date = datetime.strptime(birth_date, "%Y-%m-%d")
+    
+    try:
+        date = datetime.strptime(birth_date, "%Y-%m-%d")
 
-    if date.month > today.month:
-        print(f"\nYou will turn {today.year - date.year} this year 🎂 !")
-    else:
-        print(f"\nYou had {today.year - date.year} birthdays 🎁 !")
+        if date.month > today.month:
+            print(f"\nYou will turn {today.year - date.year} this year 🎂 !")
+        else:
+            print(f"\nYou had {today.year - date.year} birthdays 🎁 !")
 
-    print(f"\nYou are "
+        print(f"\nYou are "
           f"\n{(today - date).days} days "
           f"\n{(today.hour - date.hour)} hours "
           f"\n{(today.minute - date.minute)} minutes and"
           f"\n{(today.second - date.second)} seconds "
           f"\nold !")
+
+    except:
+        print("Invalid date")
 
 
 if __name__ == "__main__":
